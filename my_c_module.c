69f658dea6caa39732e38d33936cf2eaf99903cf
codewithmirza <manwaarullahb@gmail.com>
2024-03-23 18:19:48 +0530

    reverting c file

--- my_c_module.c
+++ my_c_module.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
-int add(int a, int b) {
-    return a + b;
+// Mark the function as exportable to make it accessible from Dart
+__attribute__((export_name("myCFunction")))
+int myCFunction(int value) {
+    printf("Received value from Dart: %d\n", value);
+    return value * 2;
 }
